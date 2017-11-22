b530bf5f99401af7abb592cf4dec29fd4ec1fbe6
Sachin Gupta <sachin.gupta@cygrp.com>
2017-11-22 18:35:29 +0530

    fixed
    db data directory path
    add ApplicationDirectory and ApplicationFile class to remove DirectoryInfo dependency

---
+++ RazorC.AdminApp/App_Code/ApplicationDirectory.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace RazorC.AdminApp.App_Code
+{
+    public static class ApplicationDirectory
+    {
+        static ApplicationDirectory()
+        {
+            var basePath = HttpContext.Current.Request.PhysicalApplicationPath;
+            var textJson = File.ReadAllText(basePath + "/ApplicationDirectory.json");
+            AppFiles = JsonConvert.DeserializeObject<ApplicationFile>(textJson);
+        }
+
+        public static ApplicationFile AppFiles { get; set; }
+        
+    }
+
+    public class ApplicationFile {
+
+        public IEnumerable<string> addOns { get; set; }
+
+        public IEnumerable<string> bodyPages { get; set; }
+
+        public IEnumerable<string> templates { get; set; }
+        public IEnumerable<string> layouts { get; set; }
+
+        public IEnumerable<MasterPageCss> masterPageCss { get; set; }
+
+
+    }
+
+    public class MasterPageCss {
+        public string Name { get; set; }
+        public string DirectoryName { get; set; }
+
+    }
+}
